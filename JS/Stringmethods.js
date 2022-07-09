eb25a0723cda8330ae79b41170c6071c65bbc58f
Fasi313 <md.fasee313@gmail.com>
2022-07-09 08:34:46 -0700

    Copied Existing code in this folder

---
+++ JS/Stringmethods.js
@@ -0,0 +1,41 @@
+    // const text= "This is my first text."
+    // console.log(text.indexOf("Everyone"));
+    // console.log(text.lastIndexOf("Everyone"));
+    // console.log(text.includes("Hello"));
+    // console.log(text.trimRight());
+    // console.log(text.trimLeft());
+    // console.log(text.trim());
+    // console.log(text.startsWith("HEllo"))
+    // console.log(text.endsWith("ones"))
+    // console.log(text.length)
+    // console.log(text.split(" "))
+    // console.log(text.charCodeAt(2))
+    // console.log(text.charAt(6))
+    // console.log(text.concat(",How Are You?"))
+    // console.log(text.toUpperCase())
+    // console.log(text.toLowerCase())
+    // console.log(text.replace("first","second"))
+    // console.log(text.slice(-13, -5))
+    // console.log(text.substring(8, 16))
+    // console.log(text.substr(8, 8))
+    // console.log(text.substr(8, 2))
+    // console.log(text.slice(0, 17))
+    // console.log(text.substring(0, 8))
+    // console.log(text.substr(0, 9))
+    // console.log(text.replace("my","your"))
+    // console.log(text.toUpperCase())
+    // console.log(text.toLowerCase())
+    // console.log(text.concat("And your?"))
+    // console.log(text.split(" "))
+    // console.log(text.trimLeft())
+    // console.log(text.charAt(2))
+    // console.log(text.length)
+    // console.log(text.startsWith("This"))
+    // console.log(text.endsWith("text."))
+    // console.log(text.includes("z"))
+    // console.log(text.indexOf("m"))
+    const num = "797";
+
+    console.log(num.padEnd(6, "313"));
+
+
---
+++ JS/numberMethods.js
@@ -0,0 +1,12 @@
+// const num = 20;
+// console.log(num.toString())
+
+// const newNum = "10.33";
+// console.log("parseInt-->",parseInt(newNum));
+// console.log("parseFloat-->",parseFloat(newNum));
+
+// const num = 0000100;
+// console.log(num);
+// console.log(undefined+undefined)
+// console.log(null+null)
+// console.log(3>2>1)
